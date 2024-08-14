08a513a95b418e53035457878069120ec8fad906
Micah Halter <micah@mehalter.com>
2024-08-14 11:45:13 -0400

    feat(repl): add `TestEnv` with `@testenv` macros in REPL

--- config/startup.jl
+++ config/startup.jl
@@ -2,9 +2,23 @@ atreplinit() do repl
     # Only setup REPL if using the REPL system image
     if haskey(ENV, "JULIA_REPL") || unsafe_string(Base.JLOptions().image_file) == joinpath(get(DEPOT_PATH, 1, joinpath(homedir(), ".julia")), "environments", "repl", "repl.so")
         @eval begin
+            # macros for utilizing test environment
+            using TestEnv
+            macro testenv()
+                return :(TestEnv.activate())
+            end
+            macro testenv(body)
+                return :(
+                    TestEnv.activate() do
+                        $body
+                    end
+                )
+            end
+            # setup OhMyREPL
             using OhMyREPL
             colorscheme!("GitHubDark")
             OhMyREPL.enable_pass!("RainbowBrackets", true)
+            # setup KittyTerminalImages
             if haskey(ENV, "KITTY_LISTEN_ON")
                 using KittyTerminalImages
                 pushKittyDisplay!()
